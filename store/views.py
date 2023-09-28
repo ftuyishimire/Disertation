0676799d6fb5a78f6c58d2bd8f7a2cc61c21352e
ftuyishimire <fabrassjoseph@gmail.com>
2023-09-28 16:19:18 +0200

    adding products and category displaying then to collection.html page

--- store/views.py
+++ store/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from .models import Category
+from django.shortcuts import redirect, render
+from django.contrib import messages
+from .models import Category, Product
 
 # Create your views here.
 
@@ -13,3 +14,16 @@ def collections(request):
         'category': category
     }
     return render(request, 'store/collections.html', context)
+
+def collectionsview(request, slug):
+    if(Category.objects.filter(slug=slug, status=0)):
+        products = Product.objects.filter(category__slug=slug)
+        category_name = Category.objects.filter(slug=slug).first()
+        context = {
+            'products': products,
+            'category_name': category_name
+        }
+        return render(request, "store/products/index.html", context)
+    else:
+        messages.warning(request, "No such category found")
+        return redirect('collections')
