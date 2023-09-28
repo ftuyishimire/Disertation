57223bc5b89318049bc6404f09495e5f6d353abe
ftuyishimire <fabrassjoseph@gmail.com>
2023-09-28 22:14:48 +0200

    cart design

--- store/urls.py
+++ store/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('collections', views.collections, name='collections'),
     path('collections/<str:slug>', views.collectionsview, name="collectionsview"),
+    path('collections/<str:cate_slug>/<str:prod_slug>', views.productview, name="productview"),
 ]
--- store/views.py
+++ store/views.py
@@ -18,12 +18,23 @@ def collections(request):
 def collectionsview(request, slug):
     if(Category.objects.filter(slug=slug, status=0)):
         products = Product.objects.filter(category__slug=slug)
-        category_name = Category.objects.filter(slug=slug).first()
-        context = {
-            'products': products,
-            'category_name': category_name
-        }
+        category = Category.objects.filter(slug=slug).first()
+        context = {'products': products,'category': category}
         return render(request, "store/products/index.html", context)
     else:
         messages.warning(request, "No such category found")
         return redirect('collections')
+
+def productview(request, cate_slug, prod_slug):
+    if(Category.objects.filter(slug=cate_slug, status=0)):
+        if(Product.objects.filter(slug=prod_slug, status=0)):
+            products = Product.objects.filter(slug=prod_slug, status=0).first
+            context = {'products': products}
+        else:
+            messages.error(request, "No such product found")
+            return redirect('collections')
+    else:
+        messages.error(request, "No such category found")
+        return redirect('collections')
+    return render(request, 'store/products/view.html', context)
+
