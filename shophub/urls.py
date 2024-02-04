cc8379e46afe3db780b13c3e37cabfa8826fe087
ftuyishimire <fabrassjoseph@gmail.com>
2024-02-04 21:18:26 +0200

    google authentication for login

--- shophub/urls.py
+++ shophub/urls.py
@@ -23,6 +23,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
+    path('auth/', include('social_django.urls', namespace='social')),
+
 ]
 
 if settings.DEBUG:
